797bf2a0fcadce53966221d20dd910364d718ad3
Emil Holmsten <emilcarlholmsten@gmail.com>
2026-02-20 17:16:41 +0100

    Font size accessability toggle

--- theme.js
+++ theme.js
@@ -5,9 +5,31 @@ function toggleTheme() {
 	localStorage.setItem("theme", next);
 }
 
+function toggleFontSize() {
+	var current = document.documentElement.getAttribute("data-font-scale");
+	if (current === "large") {
+		document.documentElement.removeAttribute("data-font-scale");
+		document.documentElement.style.removeProperty("--font-scale");
+		localStorage.removeItem("fontScale");
+	} else {
+		document.documentElement.setAttribute("data-font-scale", "large");
+		document.documentElement.style.setProperty("--font-scale", "1.15");
+		localStorage.setItem("fontScale", "large");
+	}
+}
+
 window.addEventListener("pageshow", function (event) {
 	if (event.persisted) {
 		var theme = localStorage.getItem("theme") || "light";
 		document.documentElement.setAttribute("data-theme", theme);
+
+		var fontScale = localStorage.getItem("fontScale");
+		if (fontScale === "large") {
+			document.documentElement.setAttribute("data-font-scale", "large");
+			document.documentElement.style.setProperty("--font-scale", "1.15");
+		} else {
+			document.documentElement.removeAttribute("data-font-scale");
+			document.documentElement.style.removeProperty("--font-scale");
+		}
 	}
 });
